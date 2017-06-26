5745430fa0268bd4c325477fc94f27d693ff6c42
Ramesh <gauri.ramesh@mutualofomaha.com>
2017-06-26 11:14:42 -0500

    Created overall balance button

--- src/App.js
+++ src/App.js
@@ -5,21 +5,57 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <OverallBalance placeholderValue="hello"/>
+         <OverallBalance/>
       </div>
     );
   }
 }
 
 class OverallBalance extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            overallBalance: "$0.00"
+        }
+    this.moneyIsValidated = this.moneyIsValidated.bind(this);
+    this.handleOverallBalance = this.handleOverallBalance.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+
+    };
+
     moneyIsValidated(input) {
         let regexp = /^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?\.[0-9]{1,2}$/;
-        regexp.test(input);
+        return regexp.test(input);
     }
+
+    handleOverallBalance(e) {
+        e.preventDefault();
+        console.log(this.state.overallBalance);
+        if(this.moneyIsValidated(e.target.value)===true) {
+            this.setState({overallBalance: e.target.value});
+        }
+        //may be able to condense this down into one method.
+    }
+
+    handleSubmission() {
+        document.getElementById("obalance").disabled = true;
+    }
+
+    handleEdit() {
+        //TODO: add popup functionality here.
+    }
+
+
+
+
     render() {
         return (
             <div className="App">
-                <input className="OverallBalance-input" placeholder={this.props.placeholderValue}/>
+                <form>
+                    <input id="obalance" className="OverallBalance-input" onChange={(e) => this.handleOverallBalance(e)}/>
+                    <button className="OverallBalance-submit" type="button" onClick={this.handleSubmission}> Enter </button>
+                    <button type="button" onClick={this.handleEdit}>Edit</button>
+                </form>
             </div>
         );
     }
